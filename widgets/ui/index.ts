88048fb8c36cbfaff1c790606ed56993f68ad937
Karina Grishaeva <revuwem@gmail.com>
2023-07-02 22:10:32 +0400

    feat(widgets/ui): update api

--- widgets/ui/index.ts
+++ widgets/ui/index.ts
@@ -1,3 +1,6 @@
+import LayoutHeader from "./LayoutHeader/LayoutHeader";
 import SiteNav from "./SiteNav/SiteNav";
+import Cart from "./Cart/Cart";
+import UserProfile from "./UserProfile/UserProfile";
 
-export { SiteNav };
+export { LayoutHeader, SiteNav, Cart, UserProfile };
