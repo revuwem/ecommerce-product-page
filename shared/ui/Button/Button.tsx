c9151ad86d9848ffaff5443872adaaf278bed5a9
Karina Grishaeva <revuwem@gmail.com>
2023-07-28 01:11:07 +0400

    feat(shared/ui): create Button

---
+++ shared/ui/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+const Button: React.FC<Props> = ({ children, ...props }) => (
+  <button type="button" className={styles.button} {...props}>
+    {children}
+  </button>
+);
+
+export default Button;
--- shared/ui/index.ts
+++ shared/ui/index.ts
@@ -1,4 +1,4 @@
 import Layout from "./Layout/Layout";
+import Button from "./Button/Button";
 
-// eslint-disable-next-line import/prefer-default-export
-export { Layout };
+export { Layout, Button };
