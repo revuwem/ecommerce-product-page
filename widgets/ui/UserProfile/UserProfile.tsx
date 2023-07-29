c1b5c305c3f3f9e80cd1efe1aec895124089c675
Karina Grishaeva <revuwem@gmail.com>
2023-07-29 18:29:08 +0400

    feat(widgets/ui/UserProfile): add user avatar image and styles

--- widgets/ui/UserProfile/UserProfile.tsx
+++ widgets/ui/UserProfile/UserProfile.tsx
@@ -1,3 +1,11 @@
-const UserProfile = () => <div>User</div>;
+import Image from "next/image";
+import imageAvatar from "@/public/images/image-avatar.png";
+import styles from "./UserProfile.module.css";
+
+const UserProfile = () => (
+  <div className={styles.container}>
+    <Image src={imageAvatar} alt="" fill />
+  </div>
+);
 
 export default UserProfile;
