9a69638ab09477945984dfc74fd1eaaf5c718054
Karina Grishaeva <revuwem@gmail.com>
2023-08-01 18:50:01 +0400

    feat: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { ProductDetailsT, mockProductDetailsDto } from "@/entities/product";
+import { ProductDetails } from "@/widgets/ui";
+
 export default function Home() {
-  return <main>Ecommerce Product Page</main>;
+  const product = mockProductDetailsDto(1);
+  return <ProductDetails data={product as ProductDetailsT} />;
 }
