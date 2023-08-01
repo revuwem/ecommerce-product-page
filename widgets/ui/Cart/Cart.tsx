8a64cb2b809b5f8df745054ce40e213fe19f0c71
Karina Grishaeva <revuwem@gmail.com>
2023-08-01 18:41:35 +0400

    feat(widgets/ui): create Cart

--- widgets/ui/Cart/Cart.tsx
+++ widgets/ui/Cart/Cart.tsx
@@ -1,3 +1,67 @@
-const Cart = () => <div>Cart</div>;
+"use client";
+
+import { useState } from "react";
+import CartIcon from "@/shared/assets/icons/cart.svg";
+import TrashIcon from "@/shared/assets/icons/trash.svg";
+import Image from "next/image";
+import { Button } from "@/shared/ui";
+import imgTumbnail from "../../../public/images/image-product-1-thumbnail.jpg";
+import styles from "./Cart.module.css";
+
+const Cart = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleDropdown = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  };
+
+  return (
+    <div className={styles.dropdown}>
+      <button
+        type="button"
+        className={styles.dropdownButton}
+        onClick={() => toggleDropdown()}
+      >
+        <CartIcon stroke="currentColor" width="1rem" height="1rem" />
+        <span className={styles.cartIndicator}>3</span>
+      </button>
+      {isOpen && (
+        <div className={styles.dropdownMenu}>
+          <h3>Cart</h3>
+          <div className={styles.divider} />
+          <div className={styles.dropdownContent}>
+            <ul className={styles.dropdownList}>
+              <li>
+                <div className={styles.cartItem}>
+                  <Image src={imgTumbnail} alt="" />
+                  <div>
+                    <h4>Fall Limited Edition Sneakers</h4>
+                    <p>
+                      $125.00 x 3{" "}
+                      <span className={styles.cartTotal}>$375.00</span>
+                    </p>
+                  </div>
+                  <button type="button" className={styles.cartRemoveBtn}>
+                    <TrashIcon
+                      stroke="currentColor"
+                      fill="currentColor"
+                      width="1rem"
+                      height="1rem"
+                    />
+                  </button>
+                </div>
+              </li>
+            </ul>
+            <Button variant="primary">Checkout</Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Cart;
