3d7903da1c090d18c03f99d9220c3fb787720ebe
Karina Grishaeva <revuwem@gmail.com>
2023-08-17 21:07:13 +0400

    feat(widgets/SiteNav): responsive nav

--- widgets/ui/SiteNav/SiteNav.tsx
+++ widgets/ui/SiteNav/SiteNav.tsx
@@ -1,15 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import MenuIcon from "@/shared/assets/icons/menu.svg";
+import CloseIcon from "@/shared/assets/icons/close.svg";
 import styles from "./SiteNav.module.css";
 
-const SiteNav = () => (
-  <nav>
-    <ul className={styles.navList}>
-      <li className={styles.navItem}>Collections</li>
-      <li className={styles.navItem}>Men</li>
-      <li className={styles.navItem}>Women</li>
-      <li className={styles.navItem}>About</li>
-      <li className={styles.navItem}>Contact</li>
-    </ul>
-  </nav>
-);
+const SiteNav = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleNav = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  return (
+    <>
+      <nav className={[styles.nav, isOpen ? styles.open : ""].join(" ")}>
+        <button
+          aria-label="Open Menu"
+          onClick={toggleNav}
+          className={styles.toggleButton}
+        >
+          <MenuIcon />
+        </button>
+
+        <div className={styles.navContent}>
+          <button
+            aria-label="close nav"
+            onClick={toggleNav}
+            className={styles.toggleButton}
+          >
+            <CloseIcon />
+          </button>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>Collections</li>
+            <li className={styles.navItem}>Men</li>
+            <li className={styles.navItem}>Women</li>
+            <li className={styles.navItem}>About</li>
+            <li className={styles.navItem}>Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default SiteNav;
